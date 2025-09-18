a21ab86614e546f9b20b2bdb14116643868358ab
Dereku <iam@derekunavailable.direct>
2025-09-19 04:32:55 +0700

    Seed for the ores gen

--- src/main/java/com/legacyminecraft/poseidon/PoseidonConfig.java
+++ src/main/java/com/legacyminecraft/poseidon/PoseidonConfig.java
@@ -172,6 +172,9 @@ public class PoseidonConfig extends Configuration {
         generateConfigOption("world.settings.mob-spawner-area-limit.info",
                 "This setting controls the maximum number of entities of a mob spawner type that can exist within the defined chunk radius around a mob spawner. If the number of entities exceeds this limit, the spawner will stop spawning additional entities of that type. This is useful to stop the extreme lag that can be caused by mob spawners.");
 
+        generateConfigOption("world.settings.ores-seed.enable", false);
+        generateConfigOption("world.settings.ores-seed.value", "");
+        generateConfigOption("world.settings.ores-seed.info", "This setting allows you to configure the seed for the ores generation.");
 
         //generateConfigOption("world-settings.eject-from-vehicle-on-teleport.enabled", true);
         //generateConfigOption("world-settings.eject-from-vehicle-on-teleport.info", "Eject the player from a boat or minecart before teleporting them preventing cross world coordinate exploits.");
